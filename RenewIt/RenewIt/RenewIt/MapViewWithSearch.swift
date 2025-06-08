ec0643bef04e582886312ca3a720ee2291e1ddea
RaevaDesai <122188114+RaevaDesai@users.noreply.github.com>
2025-06-08 13:15:32 -0700

    Add files via upload

---
+++ RenewIt/RenewIt/RenewIt/MapViewWithSearch.swift
@@ -0,0 +1,6 @@
+//
+//  MapViewWithSearch.swift
+//  RenewIt
+//
+//  Created by Raeva Desai on 6/8/25.
+//
---
+++ RenewIt/RenewIt/RenewIt/RenewItApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenewItApp.swift
+//  RenewIt
+//
+//  Created by Raeva Desai on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct RenewItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
